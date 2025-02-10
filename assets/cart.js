3859d2d7e656222fe74650998d69f64784a5b7de
Havock48 <adit.github@gmail.com>
2025-02-10 18:54:37 +0100

    product details page added

--- assets/cart.js
+++ assets/cart.js
@@ -1,9 +1,10 @@
 class Cart {
-    static async addToCart(variantId, quantity) {
+    static async addToCart(variant, quantity) {
+        console.log(variant)
         const data = {
             items: [
                 {
-                    id: variantId,
+                    id: variant.variantId,
                     quantity: +quantity,
                 }
             ]
@@ -16,10 +17,15 @@ class Cart {
                 body: JSON.stringify(data),
             });
 
-            if (!response.ok) throw new Error('Network response was not ok');
+            if (!response.ok) {
+                alert('No more items in stock');
+                throw new Error('Network response was not ok')
+            }
 
             const responseData = await response.json();
             console.log('Added to cart:', responseData);
+
+            showMessage(responseData?.items[0], 'success');
             return responseData;
         } catch (error) {
             console.error('Error adding to cart:', error);
--- assets/global.js
+++ assets/global.js
@@ -1,30 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
     const bestSellers = document.querySelectorAll('.best-sellers__item');
-    console.log(bestSellers)
     const selectedProducts = [];
 
     bestSellers.forEach(productElement => {
         const product = new Product(productElement);
-        const allProducts = JSON.parse(productElement.dataset.variants);
         selectedProducts.push(product);
 
         productElement.querySelectorAll('#add_to_cart').forEach(button => {
-            button.addEventListener('click', (event) => {
+            button.addEventListener('click', async (event) => {
                 event.preventDefault();
 
                 const productId = button.dataset.productId;
                 const quantity = button.dataset.quantity;
                 const product = selectedProducts.find(p => p.id === productId);
 
-                const variantId = product.variantData.find(v =>
+                const variant = product.variantData.find(v =>
                     v.size.toLowerCase() === product.sizeSelect.value.toLowerCase() &&
                     v.color.toLowerCase() === product.selectedColor.toLowerCase() &&
                     v.available
                 );
 
-                if (product) {
-                    Cart.addToCart(variantId, quantity);
-                }
+                Cart.addToCart(variant, quantity);
+
             });
         });
     });
